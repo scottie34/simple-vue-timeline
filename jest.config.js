2544bc50d0e4474bf4a1722bee2f1277ed34e9fa
Romain Latreille <latreille.romain@gmail.com>
2020-04-10 21:45:49 +0200

    ci: add test script

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel'
-}
+  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{vue, ts}',
+    '!**/node_modules/**',
+    '!<rootDir>/dist/**',
+    '!<rootDir>/src/plugins/**',
+    '!<rootDir>/src/config/**',
+    '!<rootDir>/tests/unit/**'
+  ],
+  coverageReporters: ['lcov', 'text-summary']
+};
